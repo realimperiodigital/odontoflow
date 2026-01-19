4c1a09f6edef6ceddf5c04c9fb8ad7729f59c256
Renato Silva <realimperiodigital@gmail.com>
2026-01-19 17:31:42 -0300

    chore: remove all api routes to unblock deploy

--- app/api/master/create-clinic/creat-user/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server";
-
-export async function POST() {
-  return NextResponse.json(
-    { ok: false, error: "Endpoint desativado temporariamente para build" },
-    { status: 503 }
-  );
-}
